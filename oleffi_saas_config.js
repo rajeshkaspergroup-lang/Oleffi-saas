4f21179753389302e7c51d80666ed25ce6ce070f
rajeshkaspergroup-lang <rajesh.kaspergroup@gmail.com>
2026-06-27 16:46:14 +0530

    Add files via upload

---
+++ oleffi_saas_config.js
@@ -0,0 +1,8 @@
+window.oleffiSaasConfig = {
+  apiBase: "http://127.0.0.1:8788/api",
+  salesPhone: "+91-XXXXXXXXXX",
+  salesEmail: "sales@oleffi.in",
+  companyName: "Oleffi",
+  headline: "Turn shop-floor data into daily business control.",
+  demoCtaLabel: "Show Interest"
+};
