f91fa28e926c9573fb40efc27a74bce1c7461b96
rajeshkaspergroup-lang <rajesh.kaspergroup@gmail.com>
2026-06-28 15:26:04 +0530

    Update oleffi_saas_config.js

--- oleffi_saas_config.js
+++ oleffi_saas_config.js
@@ -1,7 +1,7 @@
 window.oleffiSaasConfig = {
   apiBase: "http://127.0.0.1:8788/api",
-  salesPhone: "+91-XXXXXXXXXX",
-  salesEmail: "sales@oleffi.in",
+  salesPhone: "+91-9881069515",
+  salesEmail: "rajesh.kaspergroup@gmail.com",
   companyName: "Oleffi",
   headline: "Turn shop-floor data into daily business control.",
   demoCtaLabel: "Show Interest"
